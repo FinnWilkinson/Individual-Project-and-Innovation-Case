7ff5c14420704f12a65ad6f0abe14548fcf91fb1
MasterFinnius <finnwilkinson5@gmail.com>
2021-03-24 16:36:37 +0000

    Update CollisionPath.cs

--- HoloLensPoolTool/Assets/Scripts/CollisionPath.cs
+++ HoloLensPoolTool/Assets/Scripts/CollisionPath.cs
@@ -118,58 +118,122 @@ public class CollisionPath : MonoBehaviour
             //find intial collisions
             if (Physics.Raycast(currentPos[0], direction, out initialHits[0], Mathf.Infinity, layerMask))
             {
-                //if none hit a ball, cushion is hit so only need middle raycast
+                bool ballHitL = false, ballHitR = false, ballHitC = false;
+                bool leftSideHit = false, rightSideHit = false;
+
+                //Fire rays either side of ball to see if we will collide with another ball not down centre line
+                if (Physics.Raycast(currentPos[1], direction, out initialHits[1], Mathf.Infinity, layerMask))
+                {
+                    leftSideHit = true;
+                }
+                if (Physics.Raycast(currentPos[2], direction, out initialHits[2], Mathf.Infinity, layerMask))
+                {
+                    rightSideHit = true;
+                }
+
+                //check which rays hit balls
                 if (initialHits[0].collider.tag == "Ball")
                 {
-                    //do ball stuff
+                    ballHitC = true;
+                    Debug.Log("centre hit");
+                }
+                if (leftSideHit == true)
+                {
+                    if (initialHits[1].collider.tag == "Ball")
+                    {
+                        ballHitL = true;
+                        Debug.Log("left hit");
+                    }
+
+                }
+                if (rightSideHit == true)
+                {
+                    if (initialHits[2].collider.tag == "Ball")
+                    {
+                        ballHitR = true;
+                        Debug.Log("right hit");
+                    }
+                }
+
+                //If no ball hit, go from centre hit point as hit cushion
+                if (!ballHitL && !ballHitR && !ballHitC)
+                {
+                    //get new direction vector
+                    direction = direction - 2 * (Vector3.Dot(direction, initialHits[0].normal)) * initialHits[0].normal;
+                    dirNormal = Vector3.Cross(direction, Vector3.up);
                     //store hit point to draw path
                     hitPoints[hitsMade] = initialHits[0].point;
                     hitsMade++;
-                    break;
+                    //update current firing positions
+                    currentPos[0] = initialHits[0].point;
+                    currentPos[1] = initialHits[0].point - (dirNormal * radius);
+                    currentPos[2] = initialHits[0].point + (dirNormal * radius);
                 }
+                //else do ball collision stuff
                 else
                 {
-                    //Fire rays either side of ball to see if we will collide with another ball not down centre line
-                    bool leftSideHit = false, rightSideHit = false;
-                    if (Physics.Raycast(currentPos[1], direction, out initialHits[1], Mathf.Infinity, layerMask))
+                    // For all calculations, can emmit y as this will be the same for every ball (all same size on a flat plane)
+                    // Find closest ball which was hit
+                    List<GameObject> ballsHit = new List<GameObject>();
+                    GameObject closestBall;
+                    if (ballHitC) ballsHit.Add(initialHits[0].collider.gameObject);
+                    if (ballHitL) ballsHit.Add(initialHits[1].collider.gameObject);
+                    if (ballHitR) ballsHit.Add(initialHits[2].collider.gameObject);
+
+                    closestBall = ballsHit[0];
+                    if (ballsHit.Count > 1)
                     {
-                        leftSideHit = true;
-                    }
-                    if (Physics.Raycast(currentPos[2], direction, out initialHits[2], Mathf.Infinity, layerMask))
-                    {
-                        rightSideHit = true;
+                        Vector3 prevPoint = ball.transform.position;
+                        if (hitsMade > 0) prevPoint = hitPoints[hitsMade - 1];
+                        for (int i = 1; i < ballsHit.Count; i++)
+                        {
+                            float closestDist = Mathf.Sqrt(Mathf.Pow(closestBall.transform.position.x - prevPoint.x, 2) + Mathf.Pow(closestBall.transform.position.z - prevPoint.z, 2));
+                            float checkDist = Mathf.Sqrt(Mathf.Pow(ballsHit[i].transform.position.x - prevPoint.x, 2) + Mathf.Pow(ballsHit[i].transform.position.z - prevPoint.z, 2));
+                            if (checkDist < closestDist) closestBall = ballsHit[i];
+                        }
                     }
 
-                    bool ballHitL = false, ballHitR = false;
-                    if(leftSideHit == true)
-                    {
-                        if (initialHits[1].collider.tag == "Ball") ballHitL = true;
-                    }
-                    if(rightSideHit == true)
-                    {
-                        if (initialHits[2].collider.tag == "Ball") ballHitR = true;
-                    }
+                    // Find centre point of where cue ball will be at collision
+                    Vector3 cueBallCentre = ball.transform.position;
+                    Vector3 hitBallCentre = closestBall.transform.position;
+                    Vector3 cueBallCollCentre;
 
-                    //If no ball hit, go from centre hit point as hit cushion
-                    if(!ballHitL && !ballHitR)
-                    {
-                        //get new direction vector
-                        direction = direction - 2 * (Vector3.Dot(direction, initialHits[0].normal)) * initialHits[0].normal;
-                        dirNormal = Vector3.Cross(direction, Vector3.up);
-                        //store hit point to draw path
-                        hitPoints[hitsMade] = initialHits[0].point;
-                        hitsMade++;
-                        //update current firing positions
-                        currentPos[0] = initialHits[0].point;
-                        currentPos[1] = initialHits[0].point - (dirNormal * radius);
-                        currentPos[2] = initialHits[0].point + (dirNormal * radius);
-                    }
-                    //else do ball stuff
-                    else
-                    {
-                        break;
-                    }
+                    // Get line equation of centre ray
+                    float gradient = Mathf.Abs(cueBallCentre.z - initialHits[0].point.z) / Mathf.Abs(cueBallCentre.x - initialHits[0].point.x);
+                    if (cueBallCentre.z - initialHits[0].point.z == 0) gradient = 1;
+                    if (cueBallCentre.x - initialHits[0].point.x == 0) gradient = 0;
+                    float intercept = cueBallCentre.z - gradient * cueBallCentre.x;
 
+                    //Plug into simplified circle equation to get x intercept coords
+                    float[] potentialXcoords = new float[2], potentialZcoords = potentialXcoords = new float[2];
+                    float t = 1 + (gradient * gradient);
+                    float u = 2 * (hitBallCentre.x - (gradient * (intercept - hitBallCentre.z)));
+                    float v = (4 * (radius * radius)) - Mathf.Pow(hitBallCentre.x, 2) - Mathf.Pow(intercept - hitBallCentre.z, 2);
+                    potentialXcoords[0] = -( ( (-u) + Mathf.Sqrt((u*u) - (4*t*(-v))) ) / (2 * t));
+                    potentialXcoords[1] = -(((-u) - Mathf.Sqrt((u * u) - (4 * t * (-v)))) / (2 * t));
+                    potentialZcoords[0] = gradient * potentialXcoords[0] + intercept;
+                    potentialZcoords[1] = gradient * potentialXcoords[1] + intercept;
+
+                    if (Mathf.Sqrt(Mathf.Pow(potentialXcoords[0] - cueBallCentre.x, 2) + Mathf.Pow(potentialZcoords[0] - cueBallCentre.z, 2)) <
+                        Mathf.Sqrt(Mathf.Pow(potentialXcoords[1] - cueBallCentre.x, 2) + Mathf.Pow(potentialZcoords[1] - cueBallCentre.z, 2)))
+                        cueBallCollCentre = new Vector3(potentialXcoords[0], cueBallCentre.y, potentialZcoords[0]);
+                    else cueBallCollCentre = new Vector3(potentialXcoords[1], cueBallCentre.y, potentialZcoords[1]);
+
+                    Debug.Log("hit centre = " + hitBallCentre + ".      cue ball coll centre = " + cueBallCollCentre);
+
+                    //find point on object ball where collision occurs
+                    float ballZdiff = Mathf.Abs(cueBallCentre.z - hitBallCentre.z);
+                    float ballXdiff = Mathf.Abs(cueBallCentre.x - hitBallCentre.x);
+                    float angle = (Mathf.PI / 2) - Mathf.Asin(ballXdiff / (2 * radius));
+                    Vector3 collisionPoint = cueBallCentre + new Vector3(radius*Mathf.Sin(angle), 0.0f, radius*Mathf.Cos(angle));
+
+                    //add centre point of where cue ball will be at collision to hitPoints
+                    hitPoints[hitsMade] = cueBallCollCentre;
+                    hitsMade++;
+
+                    //calculate balls path afterwards (0 bounce)
+                    //calculate cue balls path after collision (0 bounce)
+                    break;
                 }
             }
         }
