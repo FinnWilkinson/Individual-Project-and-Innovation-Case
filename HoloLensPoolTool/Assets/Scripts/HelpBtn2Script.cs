3c0f2190e91cb43e9fb459ecf66b863b6e96fc21
MasterFinnius <finnwilkinson5@gmail.com>
2021-04-20 14:49:47 +0100

    added help screens to app

---
+++ HoloLensPoolTool/Assets/Scripts/HelpBtn2Script.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Boundary;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using Microsoft.MixedReality.Toolkit.UI;
+
+public class HelpBtn2Script : MonoBehaviour
+{
+    public GameObject helpBtn;
+    public GameObject helpBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        helpBox.SetActive(false);
+
+        // Set listener for when help btn is pressed
+        helpBtn.GetComponent<Interactable>().OnClick.AddListener(() => toggleHelp());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void toggleHelp()
+    {
+        if (helpBtn.GetComponent<Interactable>().IsToggled)
+        {
+            // If toggled, show help text box 
+            helpBox.SetActive(true);
+        }
+        else
+        {
+            // Hide help text box 
+            helpBox.SetActive(false);
+        }
+    }
+}
--- HoloLensPoolTool/Assets/Scripts/TablePlacement.cs
+++ HoloLensPoolTool/Assets/Scripts/TablePlacement.cs
@@ -27,16 +27,28 @@ public class TablePlacement : MonoBehaviour, IMixedRealityInputHandler
     public GameObject addBallBtn;
     public GameObject clearBallsBtn;
     public GameObject toggleBallUIBtn;
+    public GameObject helpBtn1;
+    public GameObject helpBtn2;
+
     public GameObject table;
+
+    public GameObject helpBox1;
+    public GameObject helpBox2;
+
 
     // Start is called before the first frame update
     void Start()
     {
         //ensure buttons are in correct starting state
         saveBtn.SetActive(true);
+        helpBtn1.SetActive(true);
         addBallBtn.SetActive(false);
         clearBallsBtn.SetActive(false);
         toggleBallUIBtn.SetActive(false);
+        helpBtn2.SetActive(false);
+
+        helpBox1.SetActive(false);
+        helpBox2.SetActive(false);
 
         //Set listeners to keep buttons with table when moved
         table.GetComponent<ManipulationHandler>().OnManipulationStarted.AddListener((input) => ManipStart(input));
@@ -52,12 +64,22 @@ public class TablePlacement : MonoBehaviour, IMixedRealityInputHandler
 
         //Set listeners for when save btn is pressed
         saveBtn.GetComponent<Interactable>().OnClick.AddListener(() => SavePos());
+
+        // Set listener for when help btn 1 is pressed
+        helpBtn1.GetComponent<Interactable>().OnClick.AddListener(() => showHelp());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Set the Focus Plane position to pool table for better Mixed Reality Capture Alighnment
+        var normal = -Camera.main.transform.forward;
+        var position = table.transform.position;
+        HolographicSettings.SetFocusPointForFrame(position, normal);
+
+        // Keep help text boxes looking at user
+        helpBox1.transform.LookAt(Camera.main.transform);
+        helpBox2.transform.LookAt(Camera.main.transform);
     }
 
     // Update Table UI transform parents when table moved so they stay relative to it
@@ -90,18 +112,28 @@ public class TablePlacement : MonoBehaviour, IMixedRealityInputHandler
     void SetParentTable()
     {
         saveBtn.transform.parent = table.transform;
+        helpBtn1.transform.parent = table.transform;
         addBallBtn.transform.parent = table.transform;
         clearBallsBtn.transform.parent = table.transform;
         toggleBallUIBtn.transform.parent = table.transform;
+        helpBtn2.transform.parent = table.transform;
+
+        helpBox1.transform.parent = table.transform;
+        helpBox2.transform.parent = table.transform;
     }
 
     //Set table UI transform parent to prefab parent game object (this)
     void SetParentThis()
     {
         saveBtn.transform.parent = this.transform;
+        helpBtn1.transform.parent = this.transform;
         addBallBtn.transform.parent = this.transform;
         clearBallsBtn.transform.parent = this.transform;
         toggleBallUIBtn.transform.parent = this.transform;
+        helpBtn2.transform.parent = this.transform;
+
+        helpBox1.transform.parent = this.transform;
+        helpBox2.transform.parent = this.transform;
     }
 
 
@@ -118,16 +150,37 @@ public class TablePlacement : MonoBehaviour, IMixedRealityInputHandler
         table.GetComponent<ConstraintManager>().enabled = false;
         table.GetComponent<BoundsControl>().enabled = false;
 
-        //dissable save btn, enable add ball btn & clear balls btn
+        // Dissable save btn and help btn 1, enable add ball btn, clear balls btn, toggle UI btn, help btn 2
         saveBtn.SetActive(false);
+        helpBtn1.SetActive(false);
         addBallBtn.SetActive(true);
         clearBallsBtn.SetActive(true);
         toggleBallUIBtn.SetActive(true);
+        helpBtn2.SetActive(true);
+
+        // Dissable help text box 1 as cant be toggled after table position is set
+        helpBox1.SetActive(false);
+
 
         //make table invisible
         foreach(Transform child in table.transform)
         {
             if(child.parent.name == table.name && child.name != "rigRoot" && child.tag != "Pocket") child.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+
+    // Toggle the help text 
+    void showHelp()
+    {
+        if (helpBtn1.GetComponent<Interactable>().IsToggled)
+        {
+            // If toggled, show help text box 1
+            helpBox1.SetActive(true);
+        }
+        else
+        {
+            // Hide help text box 1
+            helpBox1.SetActive(false);
         }
     }
 
